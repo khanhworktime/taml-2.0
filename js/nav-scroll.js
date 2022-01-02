6d774835e41c560d5793013525644d55754c6470
khanhworktime <chikhanh5@gmail.com>
2022-01-02 16:08:26 +0700

    Navbar fixed

--- js/nav-scroll.js
+++ js/nav-scroll.js
@@ -5,4 +5,16 @@ $(document).ready(function(){
           $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
         });
       });
+      
+
+      $(".navbar-toggle").click(() => {
+        let visibility = $(".navbar-toggle").get(0).getAttribute("data-visible");
+        if (visibility === "false") {
+          $(".navbar-toggle").get(0).setAttribute("data-visible", "true");
+          $(".navbar").get(0).setAttribute("aria-visible", "true");
+        } else {
+          $(".navbar-toggle").get(0).setAttribute("data-visible", "false");
+          $(".navbar").get(0).setAttribute("aria-visible", "false");
+        }
+      });
 })
